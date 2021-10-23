70b3c97c80870b3c7c8621f9d872040d616f731f
Takeru Ohta <phjgt308@gmail.com>
2021-10-23 11:08:57 +0900

    Add `ServiceBuilder::server_addr` method

--- plumcast_used/src/service.rs
+++ plumcast_used/src/service.rs
@@ -48,6 +48,14 @@ impl ServiceBuilder {
         }
     }
 
+    /// Sets the address of this server to which other peers can send messages.
+    ///
+    /// The default value is the address that has been passed to the [`ServerBuilder::new`] function.
+    pub fn server_addr(mut self, addr: SocketAddr) -> Self {
+        self.server_addr = addr;
+        self
+    }
+
     /// Sets the logger used by the service.
     ///
     /// The default value is `Logger::root(Discard, o!())`.

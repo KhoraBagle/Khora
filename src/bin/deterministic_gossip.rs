27f9b47cc2c8f0c6bf557548076072918ba86d34
RARARARARARARARARARARARA <rabramowitz6@gatech.edu>
2022-03-22 13:57:58 -0400

    now it no longer needs a power of 2

--- src/bin/deterministic_gossip.rs
+++ src/bin/deterministic_gossip.rs
@@ -30,18 +30,32 @@ fn get_children(me: usize) -> Option<[usize;2]> {
         let indicator = 1 << ledz-1;
         let you0 = me | indicator;
         let you1 = you0 ^ (indicator << 1);
-        return Some([you0,you1])
+        return Some([you1,you0])
     }
     None
+}
+fn get_real_children(me: usize, nodes: usize) -> [Option<usize>;2] {
+    if let Some(mut c) = get_children(me) {
+        while c[1] >= nodes {
+            if let Some(p) = get_children(c[1]).map(|x|x[0]) {
+                c[1] = p;
+            } else {
+                return [Some(c[0]),None]
+            }
+        }
+        return [Some(c[0]),Some(c[1])]
+    } else {
+        return [None,None]
+    }
 }
 fn main() {
     let mut rng = rand::thread_rng();
     let mut tx = [0u8;1024];
     rng.fill(&mut tx);
 
-    let nodes = 2u64.pow(10) as usize;
+    let nodes = 768usize;
     println!("{} nodes",nodes);
-    
+
     let mut me = rng.gen_range(0,nodes);
     let mut x = (0..nodes).collect::<VecDeque<_>>();
     println!("me = {}\tx[me] = {}",me,x[me]);
@@ -51,16 +65,14 @@ fn main() {
     let csize = tx.len()/8;
     let numgen = tx.chunks_exact(csize).map(|c|c.into_iter().copied().reduce(|x,y| x ^ y).unwrap()).collect::<Vec<_>>();
     
-
     let mut rng = StdRng::seed_from_u64(u64::from_le_bytes(numgen.try_into().unwrap()));
     shuffle(&mut x, &mut rng,&mut me);
     println!("me = {}\tx[me] = {}",me,x[me]);
     println!("time = {}ms",t.elapsed().as_millis());
 
-    println!("{:#064b}",me);
-    if let Some(children) = get_children(me) {
-        println!("{:#064b}",children[0]);
-        println!("{:#064b}",children[1]);
-    }
+    println!("{:#064b} = {}",me,me);
+    let children = get_real_children(me,nodes);
+    if let Some(c) = children[0] {println!("{:#064b} = {}",c,c)};
+    if let Some(c) = children[1] {println!("{:#064b} = {}",c,c)};
     println!("time = {}ms",t.elapsed().as_millis());
 }
